4d4c71acd7bbc1b63a9fe1070c1b5310e2380989
Jerry_yang <1622595386@qq.com>
2024-11-11 23:38:11 +0800

    修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
-  base: '/',
+  site: 'https://jerryym.github.io/',
+  base: '/JerryNest',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
