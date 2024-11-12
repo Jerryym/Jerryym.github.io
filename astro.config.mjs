d7b933c269989b1eb1f81dd2953b957915f7e94e
Jerry_yang <1622595386@qq.com>
2024-11-12 10:55:56 +0800

    更新deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = str => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jerryym.github.io/',
-  base: '/JerryNest',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
