44d92c3951dec774074e770dd132503c14e0d0be
Jerry_yang <1622595386@qq.com>
2024-12-10 23:33:55 +0800

    修改blog地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jerrynest.vercel.app/',
+  site: 'https://jerryym.vercel.app/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
